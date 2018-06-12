b2d4ff58c978e3966b0f190f43524c68f577a5a7
climba <mike@joomla-go.com>
2018-06-11 18:24:57 -0700

    working on login logic

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -21,5 +21,32 @@ module.exports = function(app) {
       res.json(dbAsset);
     });
   });
+
+  // app.get("/api/login", function(req, res) {
+  //   console.log("You are loging in!");
+  //   db.User.get(req.body).then(function(dbUser) {
+  //     res.json(dbUser);
+  //   });
+  // });
+
+    // GET route for getting all of the posts
+    app.post("/api/login", function (req, res) {
+      var query = {};
+      if (req.query.email) {
+        query.email = req.query.email;
+      }
+      console.log(req.body);
+      // Here we add an "include" property to our options in our findAll query
+      db.User.findAll({
+        where: {email: req.body.email},
+        include: [db.User]
+      }).then(function (dbUser) {
+        if (password === existingUser.password) {
+          console.log("YES");
+        }
+        res.json(dbUser);
+      });
+    });
+
 };
 };
