0d544b54db59d0a1baaf7497d4a3a6610642f574
climba <mike@joomla-go.com>
2018-06-13 09:38:41 -0700

    fixed login

--- models/user.js
+++ models/user.js
@@ -98,7 +98,7 @@ module.exports = function (sequelize, DataTypes) {
     //       onDelete: "cascade"
     //     });
     //   };  
-  return User;
+  
     // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
     User.prototype.validPassword = function (password) {
         return bcrypt.compareSync(password, this.password);
