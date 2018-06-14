85b630a91547e67c51078a6db4eaffdf78c238db
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2018-06-14 11:46:52 -0700

    cleaningup html, making cards work, submit button on hostregister functions properly

--- public/js/results.js
+++ public/js/results.js
@@ -2,8 +2,10 @@ $(document).ready(function () {
     $("#find_space").on("click", function (event) {
         event.preventDefault();
         var city = $("#city_finder").val();
+        // var title = $("#title_finder").val();
 
         console.log("City is: ", city);
+        // console.log("Tyype is: ", title);
 
         // This function grabs Assets from the database and updates the view
         var resultsContainer = $(".append_results");
@@ -33,9 +35,15 @@ $(document).ready(function () {
                     var newPostCardBody = $("<div>");
                     newPostCardBody.addClass("card-body");
                     var newPostBody = $("<p>");
+                    newPostBody.addClass("col-sm-6");
                     newPostTitle.text(data[i].title + " in " + data[i].city);
-                    newPostBody.html("<strong>Description: \n</strong>" + data[i].short_description);
+                    newPostBody.html("<strong>Description: </strong><br>" + data[i].short_description + 
+                                    "<br><br><br><br><br><br><strong>Price Per Day:</strong> $" + data[i].register_price_daily);
 
+                                    //WHAT I AM WORKING ON RIGHT NOW 11:45
+                    var newButtonThing = $("<button>");
+                    newButtonThing.addClass("btn-white")
+///////////////////////////////////////////////////////
                     newPostCardHeading.append(newPostTitle);
                     newPostCardBody.append(newPostCategory);
                     newPostCardBody.append(newPostBody);
