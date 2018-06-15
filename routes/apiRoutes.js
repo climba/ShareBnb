03f33e8d7438361d3b4b0630bf4ea4681f2bccdd
Mike White <mike@joomla-go.com>
2018-06-14 23:17:32 -0700

    Added new conf for login on heroku

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,6 +11,8 @@ module.exports = function (app) {
     // So we're sending the user back the route to the members page because the redirect will happen on the front end
     // They won't get this or even be able to access this page if they aren't authed
     // console.log(req.user)
+    client.end();
+    return res.end();
     res.json("/members");
     // res.render('members')
   });
